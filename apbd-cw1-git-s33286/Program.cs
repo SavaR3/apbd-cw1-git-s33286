c6d5153a7b26936b2fa285f04288b457ef921762
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-17 21:37:20 +0100

    add error to invalid user input

--- apbd-cw1-git-s33286/Program.cs
+++ apbd-cw1-git-s33286/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System.Text.RegularExpressions;
 using apbd_cw1_git_s33286;
 
-Console.WriteLine("Wprowadź liczby dla statystyki");
-String input =  Console.ReadLine();
-int[] numbers = Regex.Split(input,@"\d+")
-    .Select(x=>int.Parse(x)).ToArray();
-                    
-var statisticsHelper = new StatisticsHelper(numbers);
+try{
+    Console.WriteLine("Wprowadź liczby dla statystyki");
+    String input =  Console.ReadLine();
+    int[] numbers = Regex.Split(input,@"\d+")
+                        .Select(x=>int.Parse(x)).ToArray();
+                        
+    var statisticsHelper = new StatisticsHelper(numbers);
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: Można wprowadzać tylko liczby.");
+}
