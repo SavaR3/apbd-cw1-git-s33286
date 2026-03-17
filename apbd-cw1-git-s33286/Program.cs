a1f75cf205ba4b04b7e7dbee07de59776fcc6431
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-17 21:31:45 +0100

    add user input and new StatisticsHelper object

--- apbd-cw1-git-s33286/Program.cs
+++ apbd-cw1-git-s33286/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+using apbd_cw1_git_s33286;
+
+Console.WriteLine("Wprowadź liczby dla statystyki");
+String input =  Console.ReadLine();
+int[] numbers = Regex.Split(input,@"\d+")
+    .Select(x=>int.Parse(x)).ToArray();
+                    
+var statisticsHelper = new StatisticsHelper(numbers);
