c69033a080830e224662b78b098dd8a556e73915
Sava Racynskyi <s33286@pjwstk.edu.pl>
2026-03-17 22:11:04 +0100

    debug of regex in program class and use of calculateAverage method

--- apbd-cw1-git-s33286/Program.cs
+++ apbd-cw1-git-s33286/Program.cs
@@ -4,10 +4,13 @@ using apbd_cw1_git_s33286;
 try{
     Console.WriteLine("Wprowadź liczby dla statystyki");
     String input =  Console.ReadLine();
-    int[] numbers = Regex.Split(input,@"\d+")
+    int[] numbers = Regex.Split(input.Trim(),@"\D+")
                         .Select(x=>int.Parse(x)).ToArray();
                         
     var statisticsHelper = new StatisticsHelper(numbers);
+    
+    Console.WriteLine("śriednia wartosć twoich liczb: " + 
+                      statisticsHelper.CalculateAverage());
 }
 catch (FormatException)
 {
